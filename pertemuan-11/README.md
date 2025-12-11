7be8c157ce1a556b2fb12823d3974d06de914ac7
nuriza rahmatullah <2522500072@mahasiswa.atmaluhur.ac.id>
2025-12-11 17:46:56 +0700

    menambahkan kode baru flash message

--- pertemuan-11/README.md
+++ pertemuan-11/README.md
@@ -5,3 +5,4 @@ pada hari kamis 11 Desember 2025 saya belajar :
 <li>copy file pertemuan-10 ke pertemuan-11</li>
 <li>update fungsi.php</li>
 <li>menambahkan kode flash message dari session pada file index.php</li>
+<li>menambahkan kode baru flash message</li>
--- pertemuan-11/index.php
+++ pertemuan-11/index.php
@@ -116,9 +116,23 @@ require_once __DIR__ . '/fungsi.php';
 
     unset($_SESSION['flash_sukses'], $_SESSION['flash_error'], $_SESSION['old']); #bersihkan 3 session ini
     ?>
+
     <section id="contact">
       <h2>Kontak Kami</h2>
-      <form action="proses.php" method="POST">
+
+    <?php if (!empty($flash_sukses)): ?>
+      <div style="padding:10px; margin-bottom:10px; background:#d4edda; color:155724; border-radius:6px;">
+        <?= $flash_sukses; ?>
+    </div>
+    <?php endif; ?>
+
+     <?php if (!empty($flash_error)): ?>
+      <div style="padding:10px; margin-bottom:10px; background:#f8d7da; color:721c24; border-radius:6px;">
+        <?= $flash_error; ?>
+    </div>
+    <?php endif; ?>
+
+<form action="proses.php" method="POST">
 
         <label for="txtNama"><span>Nama:</span>
           <input type="text" id="txtNama" name="txtNama" placeholder="Masukkan nama" required autocomplete="name">
